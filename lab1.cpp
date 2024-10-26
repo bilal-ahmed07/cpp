8048ae16ba32dbddf0339dbc30545835a0dc854a
bilal-ahmed07 <bilalahmed.15952@gmail.com>
2024-10-26 19:05:48 +0500

    task 2 error fixed

--- lab1.cpp
+++ lab1.cpp
@@ -19,17 +19,26 @@
 // }
 
 //Task 2
-// #include<iostream>
-// using namespace std;
-// int main() {
-//     int a[10];
-//     for (int i = 0; i < 10; i++) {
-//         cout << "Enter number " << i + 1 << ": ";	        cin >> a[i];
-//     }
-//     for (int i = 0; i < 10; i++) {
-//        cout << a[i] << " ";
-//     }
-// }
+#include<iostream>
+using namespace std;
+int main() {
+    int a[10];
+    for (int i = 0; i < 10; i++) {
+        cout << "Enter number " << i + 1 << ": ";	        cin >> a[i];
+    }
+    for (int i = 0; i < 9; i++) {
+       for (int j= i+1; j<10; j++) {
+            if (a[i] > a[j]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << a[i] << " ";
+    }
+}
 
 
 //Task 3
@@ -53,24 +62,24 @@
 // }
 
 //Task 4
-#include <iostream>
-struct Student {
-    int id;
-    std::string name;
-    int age;
-};
-int main() {
-    Student* studt = new Student;
-    std::cout << "Enter Student ID: ";
-    std::cin >> studt->id; 
-    std::cout << "Enter Student name: ";
-    std::cin >> studt->name;
-    std::cout << "Enter Student age: ";
-    std::cin >> studt->age;
+// #include <iostream>
+// struct Student {
+//     int id;
+//     std::string name;
+//     int age;
+// };
+// int main() {
+//     Student* studt = new Student;
+//     std::cout << "Enter Student ID: ";
+//     std::cin >> studt->id; 
+//     std::cout << "Enter Student name: ";
+//     std::cin >> studt->name;
+//     std::cout << "Enter Student age: ";
+//     std::cin >> studt->age;
 
-    std::cout << "Student Id: " << studt->id
-              << "\nStudent Name: " << studt->name
-              << "\nStudent Age: " << studt->age;
-    delete studt;
-    return 0;
-}
+//     std::cout << "Student Id: " << studt->id
+//               << "\nStudent Name: " << studt->name
+//               << "\nStudent Age: " << studt->age;
+//     delete studt;
+//     return 0;
+// }
